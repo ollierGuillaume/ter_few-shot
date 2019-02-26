f858de2aabfd85aba992c2a98efab8cb2cc77f71
ollierg <guillaumeollier@hotmail.com>
2019-02-27 00:46:49 +0100

    classification semantic binary

--- prepare_omniglot.py
+++ prepare_omniglot.py
@@ -20,6 +20,7 @@ import torchvision.transforms
 import numpy as np
 from random import uniform
 from math import pi
+
 no_preprocessing = False
 # Parameters
 dataset_zip_files = ['images_background.zip', 'images_evaluation.zip']
@@ -28,35 +29,38 @@ prepared_omniglot_location = DATA_PATH + '/Omniglot/'
 output_shape = (28, 28)
 
 id_img = 0
-def handle_characters(alphabet_folder, character_folder, rotate = 0, n_variations = None):
+
+
+def handle_characters(alphabet_folder, character_folder, rotate=0, n_variations=None):
     global id_img
     for root, _, character_images in os.walk(character_folder):
         character_name = root.split('/')[-1]
-        mkdir(f'{alphabet_folder}.{rotate}/{character_name}')
+        mkdir(str(alphabet_folder) + '.' + str(rotate) + '/' + str(character_name))
         for img_path in character_images:
             # print(root+'/'+img_path)
-            img = io.imread(root+'/'+img_path)
+            img = io.imread(root + '/' + img_path)
             img = transform.rotate(img, angle=rotate)
             img = transform.resize(img, output_shape, anti_aliasing=True)
             img = (img - img.min()) / (img.max() - img.min())
-            img = 1-img
+            img = 1 - img
             if n_variations is not None:
                 for _ in range(n_variations):
-
                     basename_img = img_path.split(".")[0]
 
-                    trans = transform.AffineTransform(shear=uniform(-pi/6, pi/6),
-                                                      rotation=uniform(-pi/6, pi/6))
+                    trans = transform.AffineTransform(shear=uniform(-pi / 6, pi / 6),
+                                                      rotation=uniform(-pi / 6, pi / 6))
 
                     shift_y, shift_x = np.array(img.shape[:2]) / 2.
                     tf_shift = transform.SimilarityTransform(translation=[-shift_x, -shift_y])
                     tf_shift_inv = transform.SimilarityTransform(translation=[shift_x, shift_y])
 
                     img_trans = transform.warp(img, (tf_shift + (trans + tf_shift_inv)).inverse)
-                    io.imsave(f'{alphabet_folder}.{rotate}/{character_name}/{basename_img}.{id_img}.png', img_trans)
+                    io.imsave(str(alphabet_folder) + '.' + str(rotate) + '/' + str(character_name) + '/' + \
+                              str(basename_img) + '.' + str(id_img) + '.png', img_trans)
                     id_img += 1
             else:
-                io.imsave(f'{alphabet_folder}.{rotate}/{character_name}/{img_path}', img)
+                io.imsave(str(alphabet_folder) + '.' + str(rotate) + '/' + str(character_name) + '/' + \
+                              str(img_path), img)
             # return
 
 
@@ -66,14 +70,14 @@ def handle_alphabet(folder):
     transforms = torchvision.transforms.Compose([
         torchvision.transforms.ToPILImage(),
         torchvision.transforms.RandomAffine(20,
-                                scale=(0.9, 1.1),
-                                shear=20),
-        #torchvision.transforms.ToTensor(),
+                                            scale=(0.9, 1.1),
+                                            shear=20),
+        # torchvision.transforms.ToTensor(),
     ])
 
     for rotate in [0]:
         # Create new folders for each augmented alphabet
-        mkdir(f'{folder}.{rotate}')
+        mkdir(str(folder)+'.'+str(rotate))
         for root, character_folders, _ in os.walk(folder):
             for character_folder in character_folders:
                 # For each character folder in an alphabet rotate and resize all of the images and save
