dda850b2455b3e4547876e6e147a350e9b1c7f8e
ollierg <guillaumeollier@hotmail.com>
2019-03-25 06:35:28 +0100

    test semantic k=200 base classifier

--- tests/test_semantic_classifier.py
+++ tests/test_semantic_classifier.py
@@ -30,98 +30,97 @@ class TestSemanticBinaryClassifier(nn.Module):
 
 
 class TestSemanticClassification(unittest.TestCase):
-    # def test(self):
-    #     k = 100
-    #     n = 5
-    #     lr = 0.01
-    #     epochs = 500
-    #     size_binary_layer = 30
-    #     stochastic = False
-    #     n_conv_layers = 2
-    #
-    #     model_name = 'omniglot__n='+str(n)+'_k='+str(k)+'_epochs='+str(epochs)+'__lr=0.01__size_binary_layer='\
-    #                  +str(size_binary_layer)+('__stochastic' if stochastic else '__deterministic')\
-    #                  + ('__n_conv_layers=' + str(n_conv_layers) if n_conv_layers != 4 else "")
-    #     validation_split = .2
-    #
-    #     setup_dirs()
-    #     assert torch.cuda.is_available()
-    #
-    #     device = torch.device('cuda')
-    #     torch.backends.cudnn.benchmark = True
-    #
-    #     model = SemanticBinaryClassifier(1, k, size_binary_layer=size_binary_layer, stochastic=stochastic,
-    #                                      size_dense_layer_before_binary=None,
-    #                                      n_conv_layers=n_conv_layers)
-    #     model.load_state_dict(torch.load(os.path.join("models", "semantic_classifier",
-    #                                                   model_name+".pth")))
-    #     for param in model.parameters():
-    #         param.requires_grad = False
-    #
-    #     evaluation = OmniglotDataset('evaluation')
-    #
-    #     classes = np.random.choice(evaluation.df['class_id'].unique(), size=k)
-    #     for i in classes:
-    #         evaluation.df[evaluation.df['class_id'] == i] = evaluation.df[evaluation.df['class_id'] == i].sample(frac=1)
-    #
-    #     train_dataloader = DataLoader(
-    #         evaluation,
-    #         batch_sampler=BasicSampler(evaluation, validation_split, True, classes, n=n),
-    #         num_workers=8
-    #     )
-    #
-    #     eval_dataloader = DataLoader(
-    #         evaluation,
-    #         batch_sampler=BasicSampler(evaluation, validation_split, False, classes, n=n),
-    #         num_workers=8
-    #     )
-    #
-    #     test_model = TestSemanticBinaryClassifier(k, model, size_binary_layer=size_binary_layer)\
-    #         .to(device, dtype=torch.double)
-    #     optimiser = torch.optim.Adam(test_model.parameters(), lr=lr)
-    #     loss_fn = nn.CrossEntropyLoss().to(device)
-    #
-    #     def prepare_batch(n, k):
-    #         def prepare_batch_(batch):
-    #             x, y = batch
-    #             x = x.double().cuda()
-    #             # Create dummy 0-(num_classes - 1) label
-    #             y = create_nshot_task_label(k, n).cuda()
-    #             return x, y
-    #         return prepare_batch_
-    #
-    #     progressbar = ProgressBarLogger()
-    #     progressbar.set_params({'num_batches': k * n, 'metrics': ['categorical_accuracy'], 'loss': loss_fn,
-    #                         'verbose': 1})
-    #     evalmetrics = EvaluateMetrics(eval_dataloader)
-    #     evalmetrics.set_params({'metrics': ['categorical_accuracy'],
-    #                         'prepare_batch': prepare_batch(n, k),
-    #                         'loss_fn': loss_fn})
-    #
-    #     callbacks = [
-    #         evalmetrics,
-    #         progressbar,
-    #
-    #         ModelCheckpoint(
-    #             filepath=os.path.join(PATH, 'models', 'semantic_classifier', model_name + 'test_other_class.pth'),
-    #             monitor='val_' + str(n) + '-shot_' + str(k) + '-way_acc'
-    #         ),
-    #         ReduceLROnPlateau(patience=10, factor=0.5, monitor='val_loss'),
-    #         CSVLogger(os.path.join(PATH, 'logs', 'semantic_classifier', model_name + 'test_other_class.csv'))
-    #     ]
-    #
-    #     fit(
-    #         test_model,
-    #         optimiser,
-    #         loss_fn,
-    #         epochs=100,
-    #         dataloader=train_dataloader,
-    #         prepare_batch=prepare_batch(n, k),
-    #         callbacks=callbacks,
-    #         metrics=['categorical_accuracy'],
-    #         fit_function=gradient_step,
-    #         fit_function_kwargs={'n_shot': n, 'k_way': k, 'device': device},
-    #     )
+    def test(self):
+        k = 200
+        n = 5
+        lr = 0.01
+        epochs = 20
+        size_binary_layer = k
+        # stochastic = False
+        # n_conv_layers = 2
+
+        model_name = 'test_k=200_few_shot_classifier'
+        validation_split = .2
+
+        setup_dirs()
+        assert torch.cuda.is_available()
+
+        device = torch.device('cuda')
+        torch.backends.cudnn.benchmark = True
+
+        # model = SemanticBinaryClassifier(1, k, size_binary_layer=size_binary_layer, stochastic=stochastic,
+        #                                  size_dense_layer_before_binary=None,
+        #                                  n_conv_layers=n_conv_layers)
+        model = FewShotClassifier(1, k).to(device, dtype=torch.double)
+        model.load_state_dict(torch.load(os.path.join("models", "semantic_classifier",
+                                                      model_name+".pth")))
+        for param in model.parameters():
+            param.requires_grad = False
+
+        evaluation = OmniglotDataset('evaluation')
+
+        classes = np.random.choice(evaluation.df['class_id'].unique(), size=k)
+        for i in classes:
+            evaluation.df[evaluation.df['class_id'] == i] = evaluation.df[evaluation.df['class_id'] == i].sample(frac=1)
+
+        train_dataloader = DataLoader(
+            evaluation,
+            batch_sampler=BasicSampler(evaluation, validation_split, True, classes, n=n),
+            num_workers=8
+        )
+
+        eval_dataloader = DataLoader(
+            evaluation,
+            batch_sampler=BasicSampler(evaluation, validation_split, False, classes, n=n),
+            num_workers=8
+        )
+
+        test_model = TestSemanticBinaryClassifier(k, model, size_binary_layer=size_binary_layer)\
+            .to(device, dtype=torch.double)
+        optimiser = torch.optim.Adam(test_model.parameters(), lr=lr)
+        loss_fn = nn.CrossEntropyLoss().to(device)
+
+        def prepare_batch(n, k):
+            def prepare_batch_(batch):
+                x, y = batch
+                x = x.double().cuda()
+                # Create dummy 0-(num_classes - 1) label
+                y = create_nshot_task_label(k, n).cuda()
+                return x, y
+            return prepare_batch_
+
+        progressbar = ProgressBarLogger()
+        progressbar.set_params({'num_batches': k * n, 'metrics': ['categorical_accuracy'], 'loss': loss_fn,
+                            'verbose': 1})
+        evalmetrics = EvaluateMetrics(eval_dataloader)
+        evalmetrics.set_params({'metrics': ['categorical_accuracy'],
+                            'prepare_batch': prepare_batch(n, k),
+                            'loss_fn': loss_fn})
+
+        callbacks = [
+            evalmetrics,
+            progressbar,
+
+            ModelCheckpoint(
+                filepath=os.path.join(PATH, 'models', 'semantic_classifier', model_name + 'test_other_class.pth'),
+                monitor='val_' + str(n) + '-shot_' + str(k) + '-way_acc'
+            ),
+            ReduceLROnPlateau(patience=10, factor=0.5, monitor='val_loss'),
+            CSVLogger(os.path.join(PATH, 'logs', 'semantic_classifier', model_name + 'test_other_class.csv'))
+        ]
+
+        fit(
+            test_model,
+            optimiser,
+            loss_fn,
+            epochs=100,
+            dataloader=train_dataloader,
+            prepare_batch=prepare_batch(n, k),
+            callbacks=callbacks,
+            metrics=['categorical_accuracy'],
+            fit_function=gradient_step,
+            fit_function_kwargs={'n_shot': n, 'k_way': k, 'device': device},
+        )
     #
     # def test_view_binary(self):
     #     k = 200
@@ -177,82 +176,82 @@ class TestSemanticClassification(unittest.TestCase):
     #                     i += 1
     #             break
 
-    def test_view_conv(self):
-        n = 5
-        k = 200
-
-        setup_dirs()
-        assert torch.cuda.is_available()
-
-        device = torch.device('cuda')
-        torch.backends.cudnn.benchmark = True
-
-        model = FewShotClassifier(1, k).to(device, dtype=torch.double)
-        background = OmniglotDataset('background')
-
-        classes = np.random.choice(background.df['class_id'].unique(), size=k)
-        for i in classes:
-            background.df[background.df['class_id'] == i] = background.df[background.df['class_id'] == i].sample(frac=1)
-
-        train_dataloader = DataLoader(
-            background,
-            batch_sampler=BasicSampler(background, 0.2, True, classes, n=n),
-            num_workers=8
-        )
-
-        eval_dataloader = DataLoader(
-            background,
-            batch_sampler=BasicSampler(background, 0.2, False, classes, n=n),
-            num_workers=8
-        )
-
-        def prepare_batch(n, k):
-            def prepare_batch_(batch):
-                x, y = batch
-                x = x.double().cuda()
-                # Create dummy 0-(num_classes - 1) label
-                y = create_nshot_task_label(k, n).cuda()
-                # for e in x:
-                #     plt.imshow(e.cpu().squeeze().numpy())
-                #     plt.show()
-                return x, y
-
-            return prepare_batch_
-
-        optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
-        loss_fn = nn.CrossEntropyLoss().to(device)
-
-        evalmetrics = EvaluateMetrics(eval_dataloader)
-        evalmetrics.set_params({'metrics': ['categorical_accuracy'],
-                                'prepare_batch': prepare_batch(n, k),
-                                'loss_fn': loss_fn})
-        progressbar = ProgressBarLogger()
-        progressbar.set_params({'num_batches': k * n, 'metrics': ['categorical_accuracy'], 'loss': loss_fn,
-                                'verbose': 1})
-        callbacks = [
-            evalmetrics,
-            progressbar,
-
-            ModelCheckpoint(
-                filepath=os.path.join(PATH, 'models', 'semantic_classifier', 'test_k=300_few_shot_classifier1.pth'),
-                monitor='val_' + str(n) + '-shot_' + str(k) + '-way_acc'
-            ),
-            ReduceLROnPlateau(patience=10, factor=0.5, monitor='val_loss'),
-            CSVLogger(os.path.join(PATH, 'logs', 'semantic_classifier', 'test_k=200_few_shot_classifier.csv'))
-        ]
-
-        fit(
-            model,
-            optimiser,
-            loss_fn,
-            epochs=50,
-            dataloader=train_dataloader,
-            prepare_batch=prepare_batch(n, k),
-            callbacks=callbacks,
-            metrics=['categorical_accuracy'],
-            fit_function=gradient_step,
-            fit_function_kwargs={'n_shot': n, 'k_way': k, 'device': device},
-        )
+    # def test_view_conv(self):
+    #     n = 5
+    #     k = 200
+    #
+    #     setup_dirs()
+    #     assert torch.cuda.is_available()
+    #
+    #     device = torch.device('cuda')
+    #     torch.backends.cudnn.benchmark = True
+    #
+    #     model = FewShotClassifier(1, k).to(device, dtype=torch.double)
+    #     background = OmniglotDataset('background')
+    #
+    #     classes = np.random.choice(background.df['class_id'].unique(), size=k)
+    #     for i in classes:
+    #         background.df[background.df['class_id'] == i] = background.df[background.df['class_id'] == i].sample(frac=1)
+    #
+    #     train_dataloader = DataLoader(
+    #         background,
+    #         batch_sampler=BasicSampler(background, 0.2, True, classes, n=n),
+    #         num_workers=8
+    #     )
+    #
+    #     eval_dataloader = DataLoader(
+    #         background,
+    #         batch_sampler=BasicSampler(background, 0.2, False, classes, n=n),
+    #         num_workers=8
+    #     )
+    #
+    #     def prepare_batch(n, k):
+    #         def prepare_batch_(batch):
+    #             x, y = batch
+    #             x = x.double().cuda()
+    #             # Create dummy 0-(num_classes - 1) label
+    #             y = create_nshot_task_label(k, n).cuda()
+    #             # for e in x:
+    #             #     plt.imshow(e.cpu().squeeze().numpy())
+    #             #     plt.show()
+    #             return x, y
+    #
+    #         return prepare_batch_
+    #
+    #     optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+    #     loss_fn = nn.CrossEntropyLoss().to(device)
+    #
+    #     evalmetrics = EvaluateMetrics(eval_dataloader)
+    #     evalmetrics.set_params({'metrics': ['categorical_accuracy'],
+    #                             'prepare_batch': prepare_batch(n, k),
+    #                             'loss_fn': loss_fn})
+    #     progressbar = ProgressBarLogger()
+    #     progressbar.set_params({'num_batches': k * n, 'metrics': ['categorical_accuracy'], 'loss': loss_fn,
+    #                             'verbose': 1})
+    #     callbacks = [
+    #         evalmetrics,
+    #         progressbar,
+    #
+    #         ModelCheckpoint(
+    #             filepath=os.path.join(PATH, 'models', 'semantic_classifier', 'test_k=200_few_shot_classifier1.pth'),
+    #             monitor='val_' + str(n) + '-shot_' + str(k) + '-way_acc'
+    #         ),
+    #         ReduceLROnPlateau(patience=10, factor=0.5, monitor='val_loss'),
+    #         CSVLogger(os.path.join(PATH, 'logs', 'semantic_classifier', 'test_k=200_few_shot_classifier.csv'))
+    #     ]
+    #
+    #     fit(
+    #         model,
+    #         optimiser,
+    #         loss_fn,
+    #         epochs=50,
+    #         dataloader=train_dataloader,
+    #         prepare_batch=prepare_batch(n, k),
+    #         callbacks=callbacks,
+    #         metrics=['categorical_accuracy'],
+    #         fit_function=gradient_step,
+    #         fit_function_kwargs={'n_shot': n, 'k_way': k, 'device': device},
+    #     )
 
         # model.load_state_dict(torch.load(os.path.join("models", "semantic_classifier", "test1.pth")))
         # body_model = [i for i in model.children()][0]
