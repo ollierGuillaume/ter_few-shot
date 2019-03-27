da968a6a0f5d77e8f04ec2e5a30114619beef29f
ollierg <guillaumeollier@hotmail.com>
2019-03-27 17:08:33 +0100

    gan

--- few_shot/semantic_gan.py
+++ few_shot/semantic_gan.py
@@ -202,7 +202,7 @@ def train_dae_noisy(encoder, generator, discriminator, images, latent_size, devi
     outputs = discriminator(new_images)
     real_labels = Variable(torch.ones(images.size(0)).to(device, dtype=torch.double))
     print(outputs, real_labels)
-    dae_loss = criterionBCE(outputs, real_labels).to(device)
+    dae_loss = criterionBCE(outputs, real_labels).to(device, dtype=torch.double)
     dae_score = outputs
     dae_loss.backward()
     e_optimizer.step()
